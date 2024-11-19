7306f3d89c14937493e599280800e48d5fb05d84
rima1881 <amd.gharibipour@outlook.com>
2024-11-19 17:06:26 -0500

    initial commit

---
+++ botzilla_client.go
@@ -0,0 +1,9 @@
+package botzillaclient
+
+func Start(name string, port string) string {
+	return "f u"
+}
+
+func SendCommand(follower string, body string) string {
+	return "f u"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module botzillaclient
+
+go 1.23.1
