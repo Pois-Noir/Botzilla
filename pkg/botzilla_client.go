b43e3c40a02d147b8126aac44f39cfbbf3475a27
rima1881 <amd.gharibipour@outlook.com>
2024-11-21 21:14:34 -0500

    implemented SendCommand

--- pkg/botzilla_client.go
+++ pkg/botzilla_client.go
@@ -1,10 +1,39 @@
 package botzillaclient
 
-func Start(name string, port string) error {
+import (
+	"encoding/json"
+	"net"
+)
+
+type ports struct {
+	command int
+	message int
+	stream  int
+}
+
+func Start(name string, command int, message int, stream int) error {
 	return nil
 }
 
 func SendCommand(follower string, body string) (string, error) {
+
+	packet := map[string]string{}
+
+	packet["follower"] = follower
+	packet["body"] = body
+
+	data, err := json.Marshal(packet)
+	if err != nil {
+		return "", nil
+	}
+
+	conn, err := net.Dial("tcp", "")
+	if err != nil {
+		return "", nil
+	}
+
+	conn.Write(data)
+
 	return "", nil
 }
 
@@ -12,10 +41,10 @@ func BroadcastMessage(followers []string, body string) error {
 	return nil
 }
 
-func StartCommandListener(listener CommandListener) error {
+func StartCommandListener(listener CommandListener, port int) error {
 	return nil
 }
 
-func StartMessageListener(listener MessageListener) error {
+func StartMessageListener(listener MessageListener, port int) error {
 	return nil
 }
