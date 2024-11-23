c0f2e4cd768cb191e0ac0faaee8ea8a6928c3988
rima1881 <amd.gharibipour@outlook.com>
2024-11-22 21:58:27 -0500

    added example usage

---
+++ example/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	botzillaclient "botzillaclient/pkg"
+	"fmt"
+	"os"
+)
+
+type myListener struct{}
+
+func (l myListener) Command(body string) (string, error) {
+
+	fmt.Println("Running command listener")
+	fmt.Println(body)
+
+	return "Command Recieved", nil
+}
+
+func (l myListener) Message(body string) error {
+
+	fmt.Println("Running Message Listener")
+	fmt.Println(body)
+
+	return nil
+}
+
+func (l myListener) Stream() {
+	fmt.Println("streaming is not supported yet :O")
+}
+
+func main() {
+
+	config := botzillaclient.Config{
+		Name:        "Comp1",
+		CommandPort: 6787,
+		MessagePort: 4432,
+		StreamPort:  1212,
+	}
+
+	listener := myListener{}
+
+	token, err := botzillaclient.StartListener("localhost:8080", config, listener)
+
+	if err != nil {
+		fmt.Println("There was an error running example, ", err)
+		os.Exit(1)
+	}
+
+	println(token)
+
+	for {
+	}
+
+}
