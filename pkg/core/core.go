413747b0b988d3bf72b4e4c35d934474b9a36ec3
rima1881 <amd.gharibipour@outlook.com>
2025-06-16 16:54:24 -0400

    cleaned up bunch of stuff

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -26,28 +26,21 @@ func ConnectionHandler(conn net.Conn, key string, MessageHandler func(map[string
 	// decode it and get a header struct
 
 	var headerBuffer [global_configs.Header_LENGTH]byte
+
 	// TODO
 	// CHeck the error
 	_, err := io.ReadFull(bReader, headerBuffer[:])
 	header, err := header.Decode(bReader)
 
 	if err != nil {
-		// if there are errors we will send an appropriate response
 		// TODO create errors in the error package related to recieving and sending messages
 	}
 	// get the message length
-	requestSize := header.Length
-	// buffer to store the message
+	requestSize := header.PayloadLength
 	rawRequest := make([]byte, requestSize)
 
 	// We use io.ReadFull to guarantee that we read exactly `requestSize` bytes.
-	// A normal conn.Read may return fewer bytes than requested if the kernel buffer doesn't contain all data yet.
-	// io.ReadFull retries reads internally until the buffer is filled or an error occurs.
-	//
-	// However, if there's a transmission error (e.g. connection dropped mid-transfer),
-	// io.ReadFull may return with an error *and* partial data (n < requestSize).
-	// That's why we check `n` to verify how many bytes were actually read — not just rely on the error.
-	n, err := io.ReadFull(bReader, rawRequest[:])
+	n, err := io.ReadFull(bReader, rawRequest)
 	if n < int(requestSize) {
 
 		// TOOD create errors in the errors package related to rcving bytes
@@ -56,6 +49,7 @@ func ConnectionHandler(conn net.Conn, key string, MessageHandler func(map[string
 		// log the error
 	}
 	if err != nil {
+		// TODO
 		// do something
 	}
 
@@ -127,63 +121,6 @@ func convertToStringMap(input map[string]interface{}) map[string]string {
 	return result
 }
 
-// func Request(serverAddress string, message []byte, key []byte) ([]byte, error) {
-
-// 	// start tcp call
-// 	conn, err := net.Dial("tcp", serverAddress)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer conn.Close()
-
-// 	// Generate Header
-// 	messageLength := int32(len(message))
-// 	buf := new(bytes.Buffer)
-// 	err = binary.Write(buf, binary.LittleEndian, messageLength) // LittleEndian like umar
-// 	if err != nil {
-// 		fmt.Println("binary.Write failed:", err)
-// 		return nil, err
-// 	}
-// 	header := buf.Bytes()
-
-// 	// Generate Hash
-// 	hash := hmac.GenerateHMAC(message, key)
-
-// 	// Send token for auth
-// 	_, err = conn.Write(header)
-// 	_, err = conn.Write(message)
-// 	_, err = conn.Write(hash)
-
-// 	// TODO
-// 	// Might need better error handling here
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Reading Response Header (indicates response size)
-// 	responseHeader := [4]byte{}
-// 	_, err = conn.Read(responseHeader[:])
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Parsing Header
-// 	responseSize := int32(responseHeader[0]) |
-// 		int32(responseHeader[1])<<8 |
-// 		int32(responseHeader[2])<<16 |
-// 		int32(responseHeader[3])<<24
-
-// 	// Reading Response
-// 	rawResponse := make([]byte, responseSize)
-// 	_, err = conn.Read(rawResponse)
-
-// 	if err != nil {
-// 		fmt.Printf("Error reading from connection: %v\n", err)
-// 		return nil, err
-// 	}
-
-//		return rawResponse, nil
-//	}
 func Request(serverAddress string, payload []byte, key []byte, operationCode byte) ([]byte, error) {
 
 	// 1. Start TCP connection
